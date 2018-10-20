033df60445f58cb8b17555b4882fe6649c231d49
aroha_blue <amit42308@gmail.com>
2018-10-20 23:34:26 +0530

    [SETUP] Added package structure and API Exception

---
+++ grails-app/utils/blockdockserver/Constants.groovy
@@ -0,0 +1,17 @@
+package blockdockserver
+
+/**
+ * Created by Aroha on 20-10-2018.
+ */
+class Constants {
+    // Http Status Codes
+    class HttpCodes{
+        // 4xx
+        public static final int BAD_REQUEST                     = 400
+        public static final int UNAUTHORIZED                    = 401
+        public static final int CONFLICT                        = 409
+
+        // 5xx
+        public static final int INTERNAL_SERVER_ERROR           = 500
+    }
+}
